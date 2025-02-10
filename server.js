9e38f0abfcac07d79b72f37927bada4261b456e0
Nemozikr <ebhomrah@gmail.com>
2025-02-10 13:13:55 +0000

    Added database and made sure db.js will not be ignored in .gitignore

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const sessionsJSON = require('./database/sessions.json');
+const db = require('./database/db');
 
 const PORT = process.env.PORT || 3000;
 
